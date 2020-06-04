411267511d28480a8e1f13c46d335de21dea1912
Elaine Shaw <57796267+elshaw82@users.noreply.github.com>
2020-06-04 22:23:16 +0100

    Switch class toggle added

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const lightSwitch = document.querySelector('#switch').addEventListener('click', colorToggle);
+const lightSwitch = document.querySelector('.switch').addEventListener('click', colorToggle);
 
 
 function colorToggle(){
     var element = document.body;
     element.classList.toggle("dark-mode");
 
-    var clickSwitch = document.getElementById("switch");
-    clickSwitch.classList.toggle("reverse");
-}
+    this.classList.toggle('switch-on');
+    this.classList.toggle('switch-off');
+    // var switchButton = document.getElementById('switch');
+    // switchButton.classList.add('switch-on');
+    }
+    // var clickSwitch = document.getElementById("switch");
+    // clickSwitch.classList.toggle("reverse");
+// function classToggle() {
+//     this.classList.toggle('switch-on');
+//     this.classList.toggle('switch-off');
+// }
